52f2f4e314b9d340e263ae9a95964ad44c829d30
Dimitris Kolokouris <slimmedimitris@gmail.com>
2023-05-22 11:55:17 +0200

    koppelen database weggehaald + added contact page

--- projectdimihugo/Controllers/HomeController.cs
+++ projectdimihugo/Controllers/HomeController.cs
@@ -24,6 +24,12 @@ namespace projectdimihugo.Controllers
             return View();
         }
 
+        [Route("Contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
